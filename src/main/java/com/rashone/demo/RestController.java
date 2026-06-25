0c3a0331898c7e6a1db3fc05c140c10fb064a2ec
Gulfam Suleman <gulfam.suleman4@gmail.com>
2026-06-25 15:22:55 +0500

    Initial commit

---
+++ src/main/java/com/rashone/demo/RestController.java
@@ -0,0 +1,13 @@
+package com.rashone.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    @GetMapping("/")
+    public ResponseEntity<String> sayGi()
+    {
+        return ResponseEntity.ok("Hello Rashone");
+    }
+}
